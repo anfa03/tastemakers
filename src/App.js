374fa12b1b0a5f517186ba495c96d2624e3449be
Anfa Mohamed <anfacmohamed62@gmail.com>
2024-07-19 11:43:21 +0300

    animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import styled from "styled-components";
 import Category from "./components/Category";
 import Search from "./components/Search";
 import Pages from "./pages/Pages";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav>
+        <GiKnifeFork/>
+        <Logo to={"/"}>TastMakers</Logo>
+      </Nav>
       <Search/>
       <Category/>
       <Pages/>
@@ -15,5 +21,22 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+    text-decoration: none;
+    font-weight: 400;
+    font-size: 1.5rem;
+    font-family: 'Lobster Two', cursive;
+`;
+const Nav = styled.div`
+    padding: 4rem 0rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    svg{
+    font-size: 2rem;
+
+    }
+`;
 
 export default App;
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -19,7 +19,12 @@ function Cuisine() {
 
     },[params.type])
   return (
-    <Grid>
+    <Grid
+    animate={{opacity:1}}
+    initial={{opacity:0}}
+    exit={{opacity:0}}
+    transition={{duration:0.5}}
+    >
       {cuisine.map((item)=>{
         return(
             <Card key={item.id}>
@@ -35,12 +40,12 @@ function Cuisine() {
   )
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
       display: grid;
       grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
       grid-gap: 3rem;
 `;
-const Card = styled.div`
+const Card = styled(motion.div)`
    img{
      width: 100%;
      border-radius: 2rem;
