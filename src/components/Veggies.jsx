abdda58ccbd84bb5bcd1698f4cb9475e81d533ef
Anfa Mohamed <anfacmohamed62@gmail.com>
2024-07-30 09:09:58 +0300

    updated veggies page

--- src/components/Veggies.jsx
+++ src/components/Veggies.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import {Splide, SplideSlide} from "@splidejs/react-splide"
 import '@splidejs/react-splide/css';
+import { Link } from 'react-router-dom';
+
 
 const Veggies = () => {
 
@@ -43,9 +45,11 @@ const Veggies = () => {
             return(
               <SplideSlide key={recipe.id}>
               <Card>
+              <Link to={"/recipe/" + recipe.id}>
                 <p>{recipe.title}</p>
                 <img src={recipe.image} alt={recipe.title} />
                 <Gradient/>
+                </Link>
               </Card>
               </SplideSlide>
 
